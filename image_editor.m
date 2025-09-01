ab14686bf9f17051ef481f4df9a931b044298cd9
Tarun Choudhary <147722344+ocegik@users.noreply.github.com>
2025-09-01 05:49:33 +0000

    feat: add mode selection to image editor

--- image_editor.m
+++ image_editor.m
@@ -1,42 +1,54 @@
 function image_editor
+    choice = questdlg('Do you want to edit a single image or batch process a folder?', ...
+                      'Select Mode', ...
+                      'Single Image','Batch Folder','Single Image');
 
+    switch choice
+        case 'Single Image'
+            editSingleImage();
+        case 'Batch Folder'
+            batch_process();
+        otherwise
+            disp('No selection made. Exiting.');
+    end
+end
+
+function editSingleImage()
     [file, path] = uigetfile({'*.jpg;*.jpeg;*.png'}, 'Select an Image');
     if isequal(file,0)
         disp("File not exist. Exiting.");
         return;
     end
 
-    img = imgread(fullfile(path, file));
+    img = imread(fullfile(path, file));
     editedImg = img;
 
-    f = figure('Name', 'MATLAB Image Editor', 'Number Title', 'off',...'Position', [200 200 800 600]);
+    f = figure('Name', 'MATLAB Image Editor', 'NumberTitle', 'off', ...
+               'Position', [200 200 800 600]);
 
-    hAx = axes('Parent',f,'Position',[0.1 0.3 0.8 0.65]);
+    hAx = axes('Parent',f,'Position',[0.1 0.35 0.8 0.6]);
     hImg = imshow(img, 'Parent', hAx);
 
     uicontrol('Style','slider','Min',-100,'Max',100,'Value',0,...
-        'Units','normalized','Position',[0.1 0.2 0.7 0.04],...
+        'Units','normalized','Position',[0.1 0.25 0.7 0.04],...
         'Callback', @(src,~) updateImage());
 
     uicontrol('Style','slider','Min',0.5,'Max',2,'Value',1,...
-        'Units','normalized','Position',[0.1 0.15 0.7 0.04],...
+        'Units','normalized','Position',[0.1 0.2 0.7 0.04],...
         'Callback', @(src,~) updateImage());
 
     uicontrol('Style','pushbutton','String','Save Image',...
-        'Units','normalized','Position',[0.4 0.05 0.2 0.05],...
+        'Units','normalized','Position',[0.4 0.1 0.2 0.05],...
         'Callback', @(~,~) saveImage());
-    
+
     function updateImage()
-        % Get slider values
         sliders = findobj(f,'Style','slider');
         contrastVal = get(sliders(1),'Value');
         brightnessVal = get(sliders(2),'Value');
 
-        % Apply adjustments using helper functions
         editedImg = adjustContrast(img, contrastVal);
         editedImg = adjustBrightness(editedImg, brightnessVal);
 
-        % Update displayed image
         set(hImg,'CData',editedImg);
     end
 
